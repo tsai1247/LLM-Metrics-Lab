e1c22d58ac1f1cd2ffb777deab69852cd61234bc
tsai1247 <jeff29cc91079@gmail.com>
2025-09-01 01:46:09 +0000

    feat: download-model.sh support gguf download

--- autotest/download-model.sh
+++ autotest/download-model.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-model_path=$1
+model_path="$1"
+
+# (optional: $2 is gguf_name, maybe empty (not gguf))
+gguf_name="$2"
 
 mkdir -p ~/weights/$model_path
+if [[ -n "$gguf_name" ]]; then
+    huggingface-cli download $model_path $gguf_name --local-dir  ~/weights/$model_path --force-download
+else
+    huggingface-cli download $model_path --local-dir ~/weights/$model_path --force-download
+fi
 
-huggingface-cli download $model_path --local-dir ~/weights/$model_path --force-download
+

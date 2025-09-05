ff504ae3078c12b0c58bd63989f1baeb59decd09
tsai1247 <jeff29cc91079@gmail.com>
2025-09-05 16:44:20 +0800

    feat: update model handling to use model_owner and model_name instead of model_path

--- autotest/engine/habana-vllm/entrypoint_inner.sh
+++ autotest/engine/habana-vllm/entrypoint_inner.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo $PORT
-python -m vllm.entrypoints.openai.api_server --port=${PORT} --model ${MODEL_PATH} --served-model-name ${MODEL_NAME} --tensor-parallel-size ${TP_SIZE} &
+python -m vllm.entrypoints.openai.api_server --port=${PORT} --model ${MODEL_OWNER}/${MODEL_NAME} --served-model-name ${MODEL_NAME} --tensor-parallel-size ${TP_SIZE} &
 
 SERVER_PID=$!
 echo "Server PID: $SERVER_PID"
--- autotest/engine/habana-vllm/entrypoint_outer.sh
+++ autotest/engine/habana-vllm/entrypoint_outer.sh
@@ -1,6 +1,6 @@
 
 # 預設值 (可選)
-model_path=""
+model_owner=""
 model_name=""
 port=0
 devices=1
@@ -8,8 +8,8 @@ devices=1
 # 參數解析，支援 --arg value 和 --arg=value 兩種格式
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    --model-path) model_path="$2"; shift 2 ;;
-    --model-path=*) model_path="${1#*=}"; shift ;;
+    --model-owner) model_owner="$2"; shift 2 ;;
+    --model-owner=*) model_owner="${1#*=}"; shift ;;
     --model-name) model_name="$2"; shift 2 ;;
     --model-name=*) model_name="${1#*=}"; shift ;;
     --port) port="$2"; shift 2 ;;
@@ -21,16 +21,16 @@ while [[ $# -gt 0 ]]; do
 done
 
 # 輸出解析結果
-echo "model_path: $model_path"
+echo "model_owner: $model_owner"
 echo "model_name: $model_name"
 echo "port: $port"
 echo "devices: $devices"
 
 
-echo "run vllm server on port $port with model $model_name from $model_path using $devices devices"
+echo "run vllm server on port $port with model $model_path/$model_name using $devices devices"
 # set env PORT MODEL_PATH MODEL_NAME TP_SIZE
 export PORT=$port
-export MODEL_PATH=$model_path
+export MODEL_OWNER=$model_owner
 export MODEL_NAME=$model_name
 export TP_SIZE=$devices
 

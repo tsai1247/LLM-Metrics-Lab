8c23b03682212fc78990d20fb1176fa35b7305a1
tsai1247 <jeff29cc91079@gmail.com>
2025-09-10 14:46:14 +0800

    feat: add entrypoint script and docker-compose configuration for uifw-s-awq-ssd service

---
+++ autotest/engine/uifw-s-awq-ssd/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "engine: ${ENGINE}, model: ${MODEL_OWNER}/${MODEL_NAME}, tp_size: ${TP_SIZE}, port: ${PORT}"
+python3 -m sglang.launch_server \
+        --model-path /weights/${MODEL_OWNER}/${MODEL_NAME} \
+        --served-model-name ${MODEL_NAME} \
+        --tp-size ${TP_SIZE} \
+        --host 0.0.0.0 \
+        --port ${PORT} \
+        --enable-hierarchical-cache \
+        --hicache-size "0" \
+        --hicache-storage-backend file
+
+SERVER_PID=$!
+echo "Server PID: $SERVER_PID"
+wait $SERVER_PID
+
+

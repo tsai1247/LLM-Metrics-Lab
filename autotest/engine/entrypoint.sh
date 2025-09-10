ad756c5db54c4667eb9126968a24b7454391025d
tsai1247 <jeff29cc91079@gmail.com>
2025-09-10 10:10:21 +0800

    feat: add entrypoint script and docker-compose for Ollama service with GGUF_NAME support

--- autotest/engine/entrypoint.sh
+++ autotest/engine/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source utils/util.sh
 
-allowed_args="engine model-path model-owner model-name port tp-size"
+allowed_args="engine model-path model-owner model-name gguf-name port tp-size"
 parse_args "$allowed_args" "$@"
 
 log "start engine: $engine"
 log "\t model path: $model_path"
-log "\t model: $model_owner/$model_name"
+log "\t model: $model_owner/$model_name (/$gguf_name)"
 log "\t port: $port"
 log "\t tp_size: $tp_size"
 export ENGINE=$engine
@@ -14,6 +14,7 @@ export PORT=$port
 export MODEL_PATH=$model_path
 export MODEL_OWNER=$model_owner
 export MODEL_NAME=$model_name
+export GGUF_NAME=$gguf_name
 export TP_SIZE=$tp_size
 
-docker compose -f engine/$engine/docker-compose.yml up -d
+docker compose -f engine/$engine/docker-compose.yml up
---
+++ autotest/engine/ollama/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "engine: ${ENGINE}, model: ${MODEL_OWNER}/${MODEL_NAME}, tp_size: ${TP_SIZE}, port: ${PORT}"
+
+ollama serve &
+# wait
+echo "Waiting for Ollama server to start..."
+until ollama list &>/dev/null; do
+    sleep 1
+done
+echo "Ollama server is ready."
+
+expected="FROM /weights/${MODEL_OWNER}/${MODEL_NAME}/${GGUF_NAME}"
+echo "$expected" > /Modulefile
+
+while true; do
+  if [ -f /Modulefile ]; then
+    actual=$(cat /Modulefile)
+    if [ "$actual" = "$expected" ]; then
+      break
+    fi
+  fi
+  echo "等待 Modelfile 寫入正確內容..."
+  sleep 1
+done
+
+echo create model
+
+ollama create ${MODEL_NAME} -f /Modulefile
+
+echo run model
+
+ollama run ${MODEL_NAME}
+
+SERVER_PID=$!
+echo "Server PID: $SERVER_PID"
+wait $SERVER_PID
+
+# sleep infinity

8a36d92c2aafc44a9d5dc64f1615f06250f59a33
tsai1247 <jeff29cc91079@gmail.com>
2025-09-10 14:27:42 +0800

    feat: add entrypoint script and docker-compose configuration for uifw-l-awq service

---
+++ autotest/engine/uifw-l-awq/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "engine: ${ENGINE}, model: ${MODEL_OWNER}/${MODEL_NAME}, tp_size: ${TP_SIZE}, port: ${PORT}"
+unieai l090 \
+    --model-path /weights/${MODEL_OWNER}/${MODEL_NAME} \
+    --model-name ${MODEL_NAME} --tp ${TP_SIZE} \
+    --host 0.0.0.0 --port ${PORT} &
+
+
+SERVER_PID=$!
+echo "Server PID: $SERVER_PID"
+wait $SERVER_PID
